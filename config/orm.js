904e3330656733c0c0d3b34c251cae9fd81db79b
rezamike <michael.sanaiha@gmail.com>
2018-09-29 12:19:36 -0700

    halfway through init

---
+++ config/orm.js
@@ -0,0 +1,6 @@
+var connection = require("../config/connection.js");
+
+
+
+// Export the orm object for the model (burger.js).
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Nacho Father's Burger");
+INSERT INTO burgers (burger_name) VALUES ("Here's Lookin' at Blue, Cheese");
+INSERT INTO burgers (burger_name) VALUES ("Steak a Fork In It");
