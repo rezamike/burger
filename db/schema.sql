f76b21ee111fbb5657e529c5dddd743dc2e64a55
rezamike <michael.sanaiha@gmail.com>
2018-10-02 15:28:24 -0700

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,3 @@
-DROP DATABASE IF EXISTS burgers_db;
-
-CREATE DATABASE burgers_db;
-
 USE burgers_db;
 
 CREATE TABLE burgers (
@@ -10,5 +6,3 @@ CREATE TABLE burgers (
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
-
-SELECT * FROM burgers
